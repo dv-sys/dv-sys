efdfd8c4c25746fb44cabcd07920c63b5c74f547
iw0017 <uttam.devani@identixweb.in>
2022-05-27 19:02:22 +0530

    change in cart

--- src/Cart.js
+++ src/Cart.js
@@ -12,18 +12,15 @@ const Cart = () => {
     return (
         <>  
             <div style={{width:'100%'}}>
-                <div style={{float:'left',width:'60%'}}><h1>My Cart</h1></div>
-                <div style={{float:'left',textAlign:'right',width:'30%',padding:'8px'}}><h2>Total Item : {product.length > 0 ? product.length : 0}</h2></div>
+                <div style={{float:'left',width:'60%',marginLeft:'10px'}}><h1>My Cart</h1></div>
+                <div style={{float:'left',textAlign:'right',width:'30%',padding:'10px'}}><Button className='btn btn-warning'><Link to='/'>Back to Main</Link></Button></div>
             </div>
             <br/><br/> 
             <hr/>
-            <div style={{marginLeft:'10px'}}>
-                <Button className='btn btn-warning'><Link to='/'>Back to Main</Link></Button>
-            </div>
-            <div style={{float:'right',marginRight:'20px'}}><h4 style={{color:'green'}}>Total Price: {tprice.toFixed(2)}</h4></div>    
+            <div style={{float:'right',marginRight:'20px'}}><h2>Total Item : {product.length > 0 ? product.length : 0}</h2><h4 style={{color:'green'}}>Total Price: {tprice.toFixed(2)}</h4></div>    
             <Card className="mb-3" style={{ width: '99%', marginLeft:'5px'}}>
                 <Card.Body style={{backgroundColor:'lightgrey'}}>
-                    <div style={{width:'100%'}}>
+                    <div style={{width:'100%',fontWeight:'bold',fontSize:'14pt'}}>
                         <div style={{width:'70%',float:'left'}}>Title</div>
                         <div style={{width:'10%',float:'left'}}>Qty</div>
                         <div style={{width:'10%',float:'left'}}>Price</div>
